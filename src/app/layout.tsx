73951ff8245908900fb4ddb3b9af4153e87651b5
unknown <lokesh2006172@gmail.com>
2025-05-25 12:43:37 +0530

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,14 @@ import "./globals.css";
 import { cn, constructMetadata } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Provider from "@/components/Provider";
-import 'react-loading-skeleton/dist/skeleton.css';
+import "react-loading-skeleton/dist/skeleton.css";
 import { Toaster } from "sonner";
-import "simplebar-react/dist/simplebar.min.css"
+import "simplebar-react/dist/simplebar.min.css";
+import Head from "next/head";
 
-const inter = Inter({subsets : ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = constructMetadata()
+export const metadata: Metadata = constructMetadata();
 
 export default function RootLayout({
   children,
@@ -20,13 +21,32 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <Provider>
-      <body className={cn("min-h-screen font-sans antialiased grainy",
-        inter.className
-      )}>
-        <Toaster/>
-        <Navbar/>
-        {children}
-      </body>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+        </Head>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased grainy",
+            inter.className
+          )}
+        >
+          <Toaster />
+          <Navbar />
+          {children}
+        </body>
       </Provider>
     </html>
   );
